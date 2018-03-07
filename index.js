99d66e95e831cc922720da271d456cd2a2171788
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-03-07 03:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function sayHiToGrandma(string) {
   if (string ===string.toLowerCase())
   return("I can't hear you!");
   else if (string === string.toUpperCase())
-  return("YES INDEED!")
+  return("YES INDEED!");
+  else if(stri)
+  
 }
